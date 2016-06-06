2ad1a0cf297c15c80c5abcbcccd976b454bb2a62
doerlbh <doerlbh@gmail.com>
2016-06-05 20:50:36 -0700

    update algorithm of kNN

--- imggrider.m
+++ imggrider.m
@@ -1,6 +1,9 @@
 % developed by Baihan Lin
 % June 2016
 
+clear all;
+close all;
+
 prompt = 'Where is your pixel folder?: ';
 disp('e.g.  /Users/DoerLBH/Dropbox/git/Indie_imageGrider/test');
 path = input(prompt,'s');
@@ -26,28 +29,52 @@ keys = unique(keys);
 keys = keys(~cellfun('isempty',keys));
 keyNum = length(keys);
 
-pixw = 100;
-pixh = 100;
-keyw = 1000;
-keyh = 1000;
+pixw = 10;
+pixh = 10;
+keyw = 100;
+keyh = 100;
 
 pixspace = zeros(pixNum, 3);
 
 for pindex = 1 : pixNum
-
-origin = imread(pixs{pindex});
-I = imresize(origin,[pixw pixh],'bilinear');
-avgrgb = mean(mean(I));
-avgr = avgrgb(:,:,1);
-avgg = avgrgb(:,:,2);
-avgb = avgrgb(:,:,3);
-pixspace(pindex,:) = [avgr, avgg, avgb];
-
+    
+    origin = imread(pixs{pindex});
+    I = imresize(origin,[pixw pixh],'bilinear');
+    I = double(I);
+    avgrgb = mean(mean(I));
+    avgr = avgrgb(:,:,1);
+    avgg = avgrgb(:,:,2);
+    avgb = avgrgb(:,:,3);
+    pixspace(pindex,:) = [avgr, avgg, avgb];
+    
 end
 
-for kindex = 1 : length(keys)
-
-    origin = imread(keys{pindex});
-
-
+for kindex = 1 : keyNum
+    
+    origin = imread(keys{kindex});
+    I = imresize(origin,[keyw keyh],'bilinear');
+    I = double(I);
+    dim = size(I);
+    w = dim(1);
+    h = dim(2);
+    r = I(:,:,1);
+    g = I(:,:,2);
+    b = I(:,:,3);
+    keyspace = zeros(w, h, 1);
+    outspace = zeros(w * pixw, h * pixh, 3);
+    
+    for wind = 1:w
+        for hind = 1:h
+            keyrgb = [r(wind, hind) g(wind, hind) b(wind, hind)];
+            keyspace(w, h, 1) = knnsearch(pixspace, keyrgb);
+            for pindw = 1:pixw
+                for pindh = 1:pixh
+                    origin = imread(pixs{keyspace(w, h, 1)});
+                    I = imresize(origin,[pixw pixh],'bilinear');
+                    outspace((w-1)*pixw+pindw,(h-1)*pixh+pindh,:) = I(pindw,pindh,:);
+                end
+            end
+        end
+    end
+    figure, imshow(outspace);
 end
--- test.m
+++ test.m
@@ -10,7 +10,7 @@
 
 origin = imread('/Users/DoerLBH/Dropbox/git/Indie_imageGrider/test/key/happy-dog.jpg');
 I = imresize(origin,[100 100],'bilinear');
-I = double(I);
+% I = double(I);
 
 r = I(:,:,1);
 g = I(:,:,2);
@@ -21,8 +21,8 @@ w = dim(1);
 h = dim(2);
 
 pixspace = zeros(10, 3);
-pixspace(1,:) = [1 120 34];
 pixspace(2,:) = [170 100 134];
+pixspace(1,:) = [1 120 34];
 pixspace(3,:) = [190 12 3];
 pixspace(4,:) = [100 1 234];
 pixspace(5,:) = [10 230 24];
@@ -34,10 +34,15 @@ pixspace(9,:) = [220 230 230];
 % knnsearch(pixspace, [220 130 13])
 
 keyspace = zeros(w, h, 1);
+outspace = zeros(w, h, 3);
+
 for wind = 1:w
     for hind = 1:h
         keyrgb = [r(wind, hind) g(wind, hind) b(wind, hind)];
-        keyspace(w, h, 1) = knnsearch(pixspace, keyrgb);    
+        keyspace(w, h, 1) = knnsearch(pixspace, keyrgb);
+        outspace(w, h, :) = keyrgb;
     end
 end
 
+% imshow(I)
+imshow(outspace)
